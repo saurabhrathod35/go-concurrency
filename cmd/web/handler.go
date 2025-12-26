6e168df9e998d2dd15d7b6eca6d43367fc1d34a3
Saurabh <saurabh.rathod@lendfoundry.com>
2025-12-26 11:55:21 +0530

    Implement routing for login, logout, registration, and account activation pages

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -5,3 +5,21 @@ import "net/http"
 func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "home.page.gohtml", nil)
 }
+func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.gohtml", nil)
+}
+func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *Config) LogOutPage(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "register.page.gohtml", nil)
+}
+func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *Config) ActivateAccountPage(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *Config) routes() http.Handler {
+	// create router
+	mux := chi.NewRouter()
+
+	// set up middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(app.SessionLoad)
+
+	// define application routes
+	mux.Get("/", app.HomePage)
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.LogOutPage)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate-account", app.ActivateAccountPage)
+
+	return mux
+}
